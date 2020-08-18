5a3d44ce697e76b87d3958c2ab479ca8e10e7581
molaiea <nafissaatlagh180@gmail.com>
2020-08-18 12:37:38 +0100

    Updates

---
+++ precache-manifest.44ecdad88bfcd0217c85fd4eb47e6ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc2d8c02f5acca05294507013e4e74b",
+    "url": "/school/index.html"
+  },
+  {
+    "revision": "26229a80980f7aa6134f",
+    "url": "/school/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9072ce2540452c1ded72",
+    "url": "/school/static/js/2.140a94d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/school/static/js/2.140a94d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26229a80980f7aa6134f",
+    "url": "/school/static/js/main.be69c1c7.chunk.js"
+  },
+  {
+    "revision": "72a82b2f6e7f8bb31e9a",
+    "url": "/school/static/js/runtime-main.4771c5f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/school/static/media/logo.5d5d9eef.svg"
+  }
+]);

e0ac93a5f277843e9dc3e14cc029ee52f77dbd6b
molaiea <nafissaatlagh180@gmail.com>
2020-08-19 13:49:35 +0100

    adding files

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import tachyons from 'tachyons';
 import {Link} from 'react-router-dom';
+import './styles.css';
 
 class SignUp extends React.Component {
 
@@ -15,16 +16,21 @@ class SignUp extends React.Component {
 			parentprofession: "",
 			level: "",
 			transport: 0,
-			role: ""
+			role: "student"
 		}
 	}
 
 	handleChange = event => {
-    const { name, value } = event.target;
-
+	const {name, value} = event.target;
     this.setState({ [name]: value });
-    console.log(this.state)
+
   };
+
+  	onRadioChange=e=>{
+  		this.setState({
+  			role: e.target.value
+  		})
+  	}
 
 
 	render(){
@@ -32,104 +38,228 @@ class SignUp extends React.Component {
 
 		return(
 			<div  className="register">
-			<form className="radio">
-        <div className="">
-          <label>
-            <input type="radio" name="role" value="student" 
-            onChange={this.handleChange}/>
-            Etudiant
-          </label>
-        </div>
-        <div className="">
-          <label>
-            <input type="radio" name="role" value="teacher" 
-            onChange={this.handleChange}/>
-            Enseignant
-          </label>
-        </div>
-        <div >
-          <label>
-            <input type="radio" name="role" value="parent"
-            onChange={this.handleChange}/>
-            Parent ou Tuteur
-          </label>
-        </div>
-        <div class="custom-control custom-radio">
-        <input
-          type="radio"
-          id="customRadio1"
-          name="customRadio"
-          class="custom-control-input"
-          
-        />
-        <label class="custom-control-label" for="customRadio1"
-          >Toggle this custom radio</label
-        >
-      </div>
-      </form>
-			<form style={{ overflowY: 'scroll' ,height:'100%'}} class="hidescroll pa4 black-80 w-40 shadow-5">
-			  <div className="f1 b pb4 inscription ">Inscription</div>
-			  <div >
-			    <label className="f4 b white db mb2">Nom et Prénom </label>
-			    <input 
-			    className=" input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
-			    name="fullname"
-			    type="text" aria-describedby="name-desc"/>
-			  </div>
-			  <div>
-		    <label className="f4 b white db mb2">Addresse Email </label>
-		    <input 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
-		    name="email"
-		    type="text" aria-describedby="name-desc"/>
-		  </div>
-		  <div>
-		    <label className="f4 b white db mb2">Mot de Passe </label>
-		    <input 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
-		    name="password"
-		    type="text" aria-describedby="name-desc"/>
-		  </div>
-		  <div>
-		    <label className="f4 b white db mb2">Date de Naissance </label>
-		    <input 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
-		    name="birthday"
-		    type="date" aria-describedby="name-desc"/>
-		  </div>
-		  <div>
-		    <label class="f4 b white db mb2">Addresse </label>
-		    <input 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
-		    name="address"
-		    type="text" aria-describedby="name-desc"/>
-		  </div>
-		  <div>
-		    <label className="f4 b white db mb2">Profession du Parent <small>(ou tuteur)</small> </label>
-		    <input 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
-		    name="parentprofession"
-		    type="text" aria-describedby="name-desc"/>
-		  </div>
-		  <div >
-		    <label className="f4 b white db ">Niveau Scolaire </label>
-		    <select list="levels" 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
-			    <option value="1er">1er</option>
-			    <option value="2ème">2ème</option>
-			    <option value="3ème">3ème</option>
-			</select>
-		  </div>
-		  <div className="mb4">
-		    <label className="f4 b white db mb2">Transport </label>
-		    <select list="levels" 
-		    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
-			    <option value="1">Oui</option>
-			    <option value="0">Non</option>
-			</select>
-		  </div>
-		  <Link className="f3 ph3 pv3 black-80 bg-moon-gray shadow-5" to="/">S'inscrire</Link>
-			</form>
+			<div className="container">
+			<section class=" br4 shadow-4">
+
+			  <h1>Qui êtes-vous?</h1>
+
+			  <label>
+			    <input className="inputradio" type="radio" name="role"
+			    value="student" onChange={this.onRadioChange} checked={this.state.role==="student"}/>
+			    <span class="design"></span>
+			    <span class="text">Etudiant</span>
+			  </label>
+
+			  <label>
+			    <input className="inputradio" type="radio" name="role"
+			    value="teacher" onChange={this.onRadioChange} checked={this.state.role==="teacher"}/>
+			    <span class="design"></span>
+			    <span class="text">Enseignat</span>
+			  </label>
+
+			  <label>
+			    <input className="inputradio" type="radio" name="role"
+			    value="parent" onChange={this.onRadioChange} checked={this.state.role==="parent"}/>
+			    <span class="design"></span>
+			    <span class="text">Parent ou Tuteur</span>
+			  </label>
+
+			</section>
+			</div>
+			<div className="containerform">
+			{this.state.role==="student"?
+						<form style={{ overflowY: 'scroll' ,height:'100%'}} class="hidescroll pa4 black-80 w-60 ml5 shadow-5">
+						  <div className="f1 b pb4 inscription ">Inscription</div>
+						  <div >
+						    <label className="f4 b white db mb2">Nom et Prénom </label>
+						    <input 
+						    className=" input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+						    name="fullname"
+						    type="text" aria-describedby="name-desc"/>
+						  </div>
+						  <div>
+					    <label className="f4 b white db mb2">Addresse Email </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="email"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Mot de Passe </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="password"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Date de Naissance </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="birthday"
+					    type="date" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label class="f4 b white db mb2">Addresse </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="address"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Profession du Parent <small>(ou tuteur)</small> </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="parentprofession"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div >
+					    <label className="f4 b white db ">Niveau Scolaire </label>
+					    <select list="levels" 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
+						    <option value="1er">1er</option>
+						    <option value="2ème">2ème</option>
+						    <option value="3ème">3ème</option>
+						</select>
+					  </div>
+					  <div className="mb4">
+					    <label className="f4 b white db mb2">Transport </label>
+					    <select list="levels" 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
+						    <option value="1">Oui</option>
+						    <option value="0">Non</option>
+						</select>
+					  </div>
+					  <Link className="f3 ph3 pv3 black-80 bg-moon-gray shadow-5" to="/">S'inscrire</Link>
+						</form>
+						: this.state.role==="teacher" ? 
+						<form style={{ overflowY: 'scroll' ,height:'100%'}} class="hidescroll pa4 black-80 w-60 ml5 shadow-5">
+						  <div className="f1 b pb4 inscription ">Inscription</div>
+						  <div >
+						    <label className="f4 b white db mb2">Nom et Prénom </label>
+						    <input 
+						    className=" input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+						    name="fullname"
+						    type="text" aria-describedby="name-desc"/>
+						  </div>
+						  <div>
+					    <label className="f4 b white db mb2">Addresse Email </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="email"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Mot de Passe </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="password"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Date de Naissance </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="birthday"
+					    type="date" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label class="f4 b white db mb2">Addresse </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="address"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Profession du Parent <small>(ou tuteur)</small> </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="parentprofession"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div >
+					    <label className="f4 b white db ">Niveau Scolaire </label>
+					    <select list="levels" 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
+						    <option value="1er">1er</option>
+						    <option value="2ème">2ème</option>
+						    <option value="3ème">3ème</option>
+						</select>
+					  </div>
+					  <div className="mb4">
+					    <label className="f4 b white db mb2">Transport </label>
+					    <select list="levels" 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
+						    <option value="1">Oui</option>
+						    <option value="0">Non</option>
+						</select>
+					  </div>
+					  <Link className="f3 ph3 pv3 black-80 bg-moon-gray shadow-5" to="/">S'inscrire</Link>
+						</form>
+						: 
+						<form style={{ overflowY: 'scroll' ,height:'100%'}} class="hidescroll pa4 black-80 w-60 ml5 shadow-5">
+						  <div className="f1 b pb4 inscription ">Inscription</div>
+						  <div >
+						    <label className="f4 b white db mb2">Nom et Prénom </label>
+						    <input 
+						    className=" input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+						    name="fullname"
+						    type="text" aria-describedby="name-desc"/>
+						  </div>
+						  <div>
+					    <label className="f4 b white db mb2">Addresse Email </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="email"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Mot de Passe </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="password"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Date de Naissance </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="birthday"
+					    type="date" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label class="f4 b white db mb2">Addresse </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="address"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div>
+					    <label className="f4 b white db mb2">Profession du Parent <small>(ou tuteur)</small> </label>
+					    <input 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" 
+					    name="parentprofession"
+					    type="text" aria-describedby="name-desc"/>
+					  </div>
+					  <div >
+					    <label className="f4 b white db ">Niveau Scolaire </label>
+					    <select list="levels" 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
+						    <option value="1er">1er</option>
+						    <option value="2ème">2ème</option>
+						    <option value="3ème">3ème</option>
+						</select>
+					  </div>
+					  <div className="mb4">
+					    <label className="f4 b white db mb2">Transport </label>
+					    <select list="levels" 
+					    className="input-reset ba b--black-20 pa2 mb2 db w-100  h3 bg-white-70" name="level" id="browser">
+						    <option value="1">Oui</option>
+						    <option value="0">Non</option>
+						</select>
+					  </div>
+					  <Link className="f3 ph3 pv3 black-80 bg-moon-gray shadow-5" to="/">S'inscrire</Link>
+						</form>}
+			</div>
 			</div>
 		)
 	}
